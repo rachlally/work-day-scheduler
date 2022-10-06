1fc85b6b3513f73d8a15dce52639f8f98b020873
Dahlia <rlally89@gmail.com>
2022-10-05 20:40:05 -0700

    setStatus function attempt

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,40 @@
-var currentDate = moment().format("dddd MMMM Do, YYYY h:mm:ss a");
+//global variables
+var currentDate = moment().format("dddd, MMMM Do, YYYY h:mmA");
 $("#currentDay").text(currentDate);
+
+var currentTime = moment().hour();
+console.log(currentTime);
+
+var presentTime = document.querySelector("present");
+var futureTime = document.querySelector("future");
+var pastTime = document.querySelector("past");
+
+var hour = {9: '', 10: '', 11: '', 12: '', 13: '', 14: '', 15: '', 16: '', 17: ''};
+console.log(Object.keys(hour));
+
+
+//main code
+
+//functions
+
+//time interval
+function setStatus(){
+    for (var i = 0; i < hour.length; i++); {
+    
+    if (currentTime == hour[0]) {
+        console.log(i);
+        $('description').setAttribute('present');
+    } 
+    else if (currentTime < hour[0]) {
+        console.log(i);
+        $('description').setAttribute('future');
+    } 
+    else if (currentTime > hour[0]) {
+        console.log(i);
+        $('description').setAttribute('past');
+    }
+    }
+};
+
+setStatus();
 
